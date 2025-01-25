a3f54e4e67403d947c1912d2daff129dad2ff162
Atanas Donev <a.hristov.donev@gmail.com>
2025-01-25 14:17:49 +0100

    Hooking front-end with backend

--- src/logic/login.ts
+++ src/logic/login.ts
@@ -3,14 +3,19 @@ import axios from "axios";
 
 // Set config defaults when creating the instance
 const axiosInstance = axios.create({
-    baseURL: import.meta.env.SERVER_URL,
+    baseURL: import.meta.env.VITE_SERVER_URL,
 });
 
 export const check_credentials = async (credentials: Credentials) => {
+    console.log('Base URL:', import.meta.env.VITE_SERVER_URL);
+
     const result = await axiosInstance.post(
         "/login",
         credentials
     );
+
+    console.log(result.status);
+    console.log(result.statusText);
 
     if (result.status !== 200) {
         throw new Error("Login request failed!\nReturn code is " + result.statusText + "\n Error message: " + result.statusText);
